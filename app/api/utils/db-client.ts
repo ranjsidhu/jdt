357525d3e7c40ac38985785dbf18da4cfb8c5427
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2024-05-11 20:49:35 +0100

    feat: initial api routes

--- app/api/utils/db-client.ts
+++ app/api/utils/db-client.ts
@@ -3,4 +3,14 @@ const { createClient } = require("@supabase/supabase-js");
 const { DB_URL, DB_API_ANON_KEY } = process.env;
 const client = createClient(DB_URL, DB_API_ANON_KEY);
 
-module.exports = client;
+type CreateParams = {
+  body: { [key: string]: number | string | Date };
+  table: string;
+};
+
+const create = async ({ body, table }: CreateParams) => {
+  const { data, error } = await client.from(table).insert(body).select();
+  return { data, error };
+};
+
+export { client, create };
